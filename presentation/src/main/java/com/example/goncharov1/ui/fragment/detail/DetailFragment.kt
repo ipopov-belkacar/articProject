019d7d7d5c519620fb1290ccdcd1d82113470355
Goncharov Alexander <mr.sasha.goncharov.01@mail.ru>
2023-04-02 23:21:32 +0300

    Remove use gson

--- presentation/src/main/java/com/example/goncharov1/ui/fragment/detail/DetailFragment.kt
+++ presentation/src/main/java/com/example/goncharov1/ui/fragment/detail/DetailFragment.kt
@@ -7,13 +7,11 @@ import androidx.fragment.app.viewModels
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.goncharov1.R
 import com.example.goncharov1.databinding.FragmentDetailBinding
-import com.example.goncharov1.domain.entity.ArticEntity
 import com.example.goncharov1.viewmodels.DetailViewModel
-import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.scopes.FragmentScoped
 
-private const val ARG_PARAM_ARTIC_ITEM = "paramArticItem"
+private const val ARG_PARAM_ARTIC_ID = "paramArticId"
 
 @FragmentScoped
 @AndroidEntryPoint
@@ -21,48 +19,47 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private val binding: FragmentDetailBinding by viewBinding()
     private val viewModel: DetailViewModel by viewModels()
-    private var articItem: ArticEntity? = null
+    private var articId: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            articItem = Gson().fromJson(it.getString(ARG_PARAM_ARTIC_ITEM), ArticEntity::class.java)
+            articId = it.getInt(ARG_PARAM_ARTIC_ID)
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        with(binding) {
-            textArtistDisplay.text =
-                getString(R.string.artist_display_template, articItem?.artistDisplay)
-            textTitle.text = getString(R.string.title_display_template, articItem?.title)
-        }
 
         viewModel.downloadImageLiveData.observe(viewLifecycleOwner) {
             it.into(binding.mainImage)
         }
 
         viewModel.getArticEntityLiveData.observe(viewLifecycleOwner) {
-            println("!!!!!!!!!!!$it")
-        }
+            with(binding) {
+                textArtistDisplay.text =
+                    getString(R.string.artist_display_template, it.artistDisplay)
+                textTitle.text = getString(R.string.title_display_template, it.title)
+            }
 
-        articItem?.imageId.let {
             viewModel.downloadImage(
-                requireContext().getString(R.string.main_url_for_upload_image, it),
+                requireContext().getString(R.string.main_url_for_upload_image, it.imageId),
                 R.drawable.image_placeholder
             )
         }
 
-        viewModel.getArticById(articItem!!.id)
+        articId?.let {
+            viewModel.getArticById(it)
+        }
     }
 
     companion object {
         @JvmStatic
         fun newInstance(
-            itemArtic: String
+            articId: Int
         ) = DetailFragment().apply {
             arguments = Bundle().apply {
-                putString(ARG_PARAM_ARTIC_ITEM, itemArtic)
+                putInt(ARG_PARAM_ARTIC_ID, articId)
             }
         }
     }
